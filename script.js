985191a0d754fef4ef0029254f8d75de6055bff1
SRIRAM VISWANATHAN <117807050+iam-srv@users.noreply.github.com>
2023-06-10 12:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Function to handle window resize event
+function dimention() {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  
+  let dimensionsDiv = document.getElementById('container');
+  dimensionsDiv.textContent = "Width: " + width + ", Height: " + height;
+  
+}
+window.addEventListener('resize', dimention);
+
+
+dimention();
